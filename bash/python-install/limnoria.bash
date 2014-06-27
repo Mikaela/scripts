d6e59936ce66e91a3fa4548291bfa48d428ec6c4
Mikaela Suomalainen <mikaela.suomalainen@outlook.com>
2014-06-27 12:14:27 +0300

    Add limnoria.bash to install Limnoria using pip.

---
+++ bash/python-install/limnoria.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+if [[ $USER != "root" ]]; then
+    NOROOT="--user"
+fi
+
+pip install -r https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.txt $NOROOT $1
+## VERY EXPERIMENTAL, BROKEN AND DOES NOT EXIST!
+#pip install -r https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.vcs.txt $NOROOT $1
+pip install git+https://github.com/ProgVal/Limnoria.git@master $NOROOT $1
