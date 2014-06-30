e505a63cbcbb00066363fabd160d41e43a289605
Mikaela Suomalainen <mikaela.suomalainen@outlook.com>
2014-06-30 15:21:29 +0300

    limnoria.bash: add `--upgrade`
    If something isn't installed, it will be installed and if something
    is installed, but outdated, it will be upgraded and if it's up-to-date
    nothing is done.

--- bash/python-install/limnoria.bash
+++ bash/python-install/limnoria.bash
@@ -3,7 +3,7 @@ if [[ $USER != "root" ]]; then
     NOROOT="--user"
 fi
 
-pip install -r https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.txt $NOROOT $1
+pip install -r https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.txt --upgrade $NOROOT $1
 ## VERY EXPERIMENTAL, BROKEN AND DOES NOT EXIST!
-#pip install -r https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.vcs.txt $NOROOT $1
-pip install git+https://github.com/ProgVal/Limnoria.git@master $NOROOT $1
+#pip install -r https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.vcs.txt --upgrade $NOROOT $1
+pip install git+https://github.com/ProgVal/Limnoria.git@master --upgrade $NOROOT $1
