3c4aeb53208f64dd3352b9f8dd3ed8ae6f1931a9
Mikaela Suomalainen <mikaela.suomalainen@outlook.com>
2014-07-12 16:05:39 +0300

    limnoria: force Python 3

--- bash/python-install/limnoria.bash
+++ bash/python-install/limnoria.bash
@@ -3,10 +3,10 @@ if [[ $USER != "root" ]]; then
     NOROOT="--user"
 fi
 
-pip install -vr https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.txt --upgrade $NOROOT $1
+pip3 install -vr https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.txt --upgrade $NOROOT $1
 ## VERY EXPERIMENTAL, BROKEN AND DOES NOT EXIST!
-#pip install -vr https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.vcs.txt --upgrade $NOROOT $1
-pip install -v git+https://github.com/ProgVal/Limnoria.git@master --upgrade $NOROOT $1
+#pip3 install -vr https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.vcs.txt --upgrade $NOROOT $1
+pip3 install -v git+https://github.com/ProgVal/Limnoria.git@master --upgrade $NOROOT $1
 
 # ProgVal/Supybot-plugins
-pip install -vr https://raw.githubusercontent.com/ProgVal/Supybot-plugins/master/requirements.txt --upgrade $NOROOT $1
+pip3 install -vr https://raw.githubusercontent.com/ProgVal/Supybot-plugins/master/requirements.txt --upgrade $NOROOT $1
