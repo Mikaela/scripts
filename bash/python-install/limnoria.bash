db2cafa8b13c545fb23547abac5be19fdb5c7498
Mikaela Suomalainen <mikaela.suomalainen@outlook.com>
2014-07-12 14:55:23 +0300

    limnoria*: be more verbose

--- bash/python-install/limnoria.bash
+++ bash/python-install/limnoria.bash
@@ -3,10 +3,10 @@ if [[ $USER != "root" ]]; then
     NOROOT="--user"
 fi
 
-pip install -r https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.txt --upgrade $NOROOT $1
+pip install -vr https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.txt --upgrade $NOROOT $1
 ## VERY EXPERIMENTAL, BROKEN AND DOES NOT EXIST!
-#pip install -r https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.vcs.txt --upgrade $NOROOT $1
-pip install git+https://github.com/ProgVal/Limnoria.git@master --upgrade $NOROOT $1
+#pip install -vr https://raw.githubusercontent.com/ProgVal/Limnoria/master/requirements.vcs.txt --upgrade $NOROOT $1
+pip install -v git+https://github.com/ProgVal/Limnoria.git@master --upgrade $NOROOT $1
 
 # ProgVal/Supybot-plugins
-pip install -r https://raw.githubusercontent.com/ProgVal/Supybot-plugins/master/requirements.txt --upgrade $NOROOT $1
+pip install -vr https://raw.githubusercontent.com/ProgVal/Supybot-plugins/master/requirements.txt --upgrade $NOROOT $1
