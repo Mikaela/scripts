2636dd06cfb2b7cc6432a94395d3bdd562109417
Mikaela Suomalainen <mikaela.suomalainen@outlook.com>
2014-09-24 19:00:24 +0300

    update znc*.bash

--- bash/zncconfigure.bash
+++ bash/zncconfigure.bash
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+set -x
+unset CC
+unset CXX
 cd znc
 ./autogen.sh
 mkdir -p build
 cd build
 # PYENV (two lines)
-export LD_LIBRARY_PATH=/home/users/mkaysi/.pyenv/versions/3.4.1/lib
-export PKG_CONFIG_PATH=/home/users/mkaysi/.pyenv/versions/3.4.1/lib/pkgconfig/
-../configure --enable-debug --enable-perl --enable-python --enable-swig --enable-tcl --disable-cyrus --prefix=$HOME/.local
-make -j$(nproc)
+#export LD_LIBRARY_PATH=/home/users/mkaysi/.pyenv/versions/3.4.1/lib
+#export PKG_CONFIG_PATH=/home/users/mkaysi/.pyenv/versions/3.4.1/lib/pkgconfig/
+../configure --enable-debug --enable-perl --enable-python --enable-swig --enable-tcl --enable-cyrus --prefix=$HOME/.local
+#make -j$(nproc)
--- bash/zncmodules.bash
+++ bash/zncmodules.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # This script downloads external ZNC modules which I use.
+set -x
 cd znc
 cd modules
 echo "Downloading kvirc.cpp"
@@ -8,6 +9,11 @@ curl -LO https://raw.githubusercontent.com/DarthGandalf/znc/dgmods/modules/kvirc
 #curl -LO https://raw.githubusercontent.com/mmilata/znc-otr/master/otr.cpp
 echo "Downloading privmsg.cpp"
 curl -LO https://raw.githubusercontent.com/kylef/znc-contrib/master/privmsg.cpp
+echo "Downloading away_notify.cpp"
+curl -LO https://github.com/NuclearW/znc-modules/raw/master/away_notify/away_notify.cpp
+echo "Downloading away_notify_unsolicited.cpp"
+curl -LO https://github.com/NuclearW/znc-modules/raw/master/away_notify/away_notify_unsolicited.cpp
+
 
 # ~/.znc/modules
 mkdir -p ~/.znc/modules
@@ -16,4 +22,5 @@ echo "Downloading push.py"
 curl -LO https://raw.githubusercontent.com/jreese/znc-push/python/push.py
 
 echo "You can now run zncconfigure.bash"
+
 
