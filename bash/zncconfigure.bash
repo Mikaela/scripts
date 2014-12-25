a73366348b35b151379bff12b568ccbed1e1caee
Mikaela Suomalainen <mikaela.suomalainen@outlook.com>
2014-12-25 16:22:01 +0200

    bash: update znc*

--- bash/zncconfigure.bash
+++ bash/zncconfigure.bash
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 set -x
-unset CC
-unset CXX
+
+## Debian 7 has too old clang so use this with it
+#unset CC
+#unset CXX
+
 cd znc
 ./autogen.sh
 mkdir -p build
 cd build
-# PYENV (two lines)
-#export LD_LIBRARY_PATH=/home/users/mkaysi/.pyenv/versions/3.4.1/lib
-#export PKG_CONFIG_PATH=/home/users/mkaysi/.pyenv/versions/3.4.1/lib/pkgconfig/
-../configure --enable-debug --enable-perl --enable-python --enable-swig --enable-tcl --enable-cyrus --prefix=$HOME/.local
+
+## for use with pyenv
+#export LD_LIBRARY_PATH=/home/users/mkaysi/.pyenv/versions/3.4.2/lib
+#export PKG_CONFIG_PATH=/usr/bin/pkg-config:/home/users/mkaysi/.pyenv/versions/3.4.2/lib/pkgconfig/:/usr/lib/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig/:/usr/share/pkgconfig/
+
+../configure --enable-debug --enable-perl --enable-python --enable-swig --enable-tcl --enable-cyrus
+
+#--prefix=$HOME/.local
 #make -j$(nproc)
--- bash/zncmodules.bash
+++ bash/zncmodules.bash
@@ -3,24 +3,12 @@
 set -x
 cd znc
 cd modules
-echo "Downloading kvirc.cpp"
 curl -LO https://raw.githubusercontent.com/DarthGandalf/znc/dgmods/modules/kvirc.cpp
-#echo "Downloading otr.cpp"
-#curl -LO https://raw.githubusercontent.com/mmilata/znc-otr/master/otr.cpp
-echo "Downloading privmsg.cpp"
 curl -LO https://raw.githubusercontent.com/kylef/znc-contrib/master/privmsg.cpp
-echo "Downloading away_notify.cpp"
 curl -LO https://github.com/NuclearW/znc-modules/raw/master/away_notify/away_notify.cpp
-echo "Downloading away_notify_unsolicited.cpp"
 curl -LO https://github.com/NuclearW/znc-modules/raw/master/away_notify/away_notify_unsolicited.cpp
 
-
-# ~/.znc/modules
+# ~/.znc/modules for mod* modules
 mkdir -p ~/.znc/modules
 cd ~/.znc/modules
-echo "Downloading push.py"
 curl -LO https://raw.githubusercontent.com/jreese/znc-push/python/push.py
-
-echo "You can now run zncconfigure.bash"
-
-
