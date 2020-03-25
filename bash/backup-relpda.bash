71d6a47cee0615163bf574a0f1897b5e700fa676
Mikaela Suomalainen <mikaela+git@mikaela.info>
2020-03-25 14:57:28 +0200

    backup-relpda: recursive, mumble, verbosity

--- bash/backup-relpda.bash
+++ bash/backup-relpda.bash
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
-
+set -x
 TARGETDIR=/home/mikaela/backups/
 
 mkdir -p $TARGETDIR
 
-rsync -av /home/znc/.znc/ $TARGETDIR/znc
+rsync -avr /home/znc/.znc/ $TARGETDIR/znc
 
 mkdir -p $TARGETDIR/bitbot/
-rsync -av /home/bitbot/bitbot/bot.conf /home/bitbot/bitbot/databases/bot.db $TARGETDIR/bitbot/
-rsync -av /home/bitbot/.bitbot/ $TARGETDIR/bitbot/dotbitbot/
+rsync -avr /home/bitbot/bitbot/bot.conf /home/bitbot/bitbot/databases/bot.db $TARGETDIR/bitbot/
+rsync -avr /home/bitbot/.bitbot/ $TARGETDIR/bitbot/dotbitbot/
 
 # Think about ssh keys? But I don't rotate hostnames with VPSes...
-rsync -av /etc/yggdrasil.conf $TARGETDIR
+rsync -avr /etc/yggdrasil.conf $TARGETDIR
 
-rsync -av /etc/systemd/system/syncplay-server.service $TARGETDIR
+rsync -avr /etc/systemd/system/syncplay-server.service $TARGETDIR
 
 mkdir -p $TARGETDIR/mumble/ssl/
-rsync -av /etc/mumble-server.ini /var/lib/mumble-server/mumble-server.sqlite $TARGETDIR/mumble/
-rsync /var/lib/mumble-server/ssl/ $TARGETDIR/mumble/ssl
+rsync -avr /etc/mumble-server.ini /var/lib/mumble-server/mumble-server.sqlite $TARGETDIR/mumble/
+rsync -avr /var/lib/mumble-server/ssl/ $TARGETDIR/mumble/ssl
 
-rsync -av /etc/oidentd.conf $TARGETDIR
+rsync -avr /etc/oidentd.conf $TARGETDIR
 
 mkdir -p $TARGETDIR/thelounge/
-rsync -av /etc/thelounge/ $TARGETDIR/thelounge/
+rsync -avr /etc/thelounge/ $TARGETDIR/thelounge/
 
 mkdir -p $TARGETDIR/matterbridge/
-rsync -av /home/matterbridge/config/matterbridge.toml $TARGETDIR/matterbridge
-rsync -av /etc/systemd/system/matterbridge.service $TARGETDIR/matterbridge/matterbridge.service
+rsync -avr /home/matterbridge/config/matterbridge.toml $TARGETDIR/matterbridge
+rsync -avr /etc/systemd/system/matterbridge.service $TARGETDIR/matterbridge/matterbridge.service
 
 chown -R mikaela:mikaela $TARGETDIR
 chmod -R g-rxw,o-rxw $TARGETDIR
+set +x
