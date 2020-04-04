f5043dfe955b18e4a606afa5b28076826038992f
Mikaela Suomalainen <mikaela+git@mikaela.info>
2020-04-04 10:52:19 +0300

    bash: add comms.bash for launching communication apps

---
+++ bash/comms.bash
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Opening the communication apps with a single command without having to
+# get them from dmenu so much. Excludes Firefox and Thunderbird as I have
+# them inconsistently installed across systems
+
+set -x
+
+# XMPP
+(dino&)
+(flatpak run org.gajim.Gajim --quiet&)
+
+# Jami, mostly P2P app
+(jami-gnome -r&)
+
+# Telegram.org
+(telegram-desktop -many -startintray&)
+mkdir -p ~/.config/T2L-telegram
+(telegram-desktop -many -workdir ~/.config/T2L-telegram -startintray&)
+
+# Matrix - not in use in favour of Radical
+#(flatpak run io.github.NhekoReborn.Nheko&)
+#(flatpak run im.riot.Riot --hidden)
+#(flatpak run im.riot.Riot --profile T2L --hidden --proxy --proxy-server=127.0.0.1:9119&)
+
+# Keybase
+(KEYBASE_AUTOSTART=1 run_keybase -a&)
+
+# Steam
+(STEAM_FRAME_FORCE_CLOSE=1 steam -silent&)
+# WireDesktop - doesn't have --startup due to a bug that it may hide notifications launched that way
+(flatpak run com.wire.WireDesktop&)
+
+# Signal
+(flatpak run org.signal.Signal --start-in-tray&)
+
+set +x
