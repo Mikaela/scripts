c9a06a6567de7ad4e76cc6405a5bc1010404f43a
Mikaela Suomalainen <mikaela+git@mikaela.info>
2021-05-16 18:06:09 +0300

    borgbackup-etro.bash: include /home/limnoria/R-66Y

--- bash/borgbackup-etro.bash
+++ bash/borgbackup-etro.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export BORG_REPO='/root/borgbackups-etro.mikaela.info'
 
-borg create ::$(date -Is) /home/bitbot/.bitbot /home/matterbridge/ /home/oragono/oragono-conf /home/mikaela/.weechat
+borg create ::$(date -Is) /home/bitbot/.bitbot /home/matterbridge/ /home/oragono/oragono-conf /home/mikaela/.weechat /home/limnoria/R-66Y
 
 rm -rf /home/mikaela/backups/
 cp -r $BORG_REPO /home/mikaela/backups/
