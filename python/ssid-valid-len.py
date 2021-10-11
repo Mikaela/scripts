ef5dd083cba8da245e90a5203ba83622fed0eaee
Mikaela Suomalainen <mikaela+digitalents@mikaela.info>
2021-10-11 10:51:25 +0300

    ssid-valid-len.py: attempt adding _nomap counting
    Ref: #31

--- python/ssid-valid-len.py
+++ python/ssid-valid-len.py
@@ -5,15 +5,26 @@ This script asks for a SSID name, counts it and tells whether it's of valid
 length
 '''
 
-# Request input
-givenssid = input("Please enter the SSID you are thinking of: ")
-#givenssid = "test_nomap"
+# Request input or assume "test" on empty.
+givenssid = str(input("Please enter the SSID you are thinking of (preferably 26 chars to fit _nomap): ") or "test")
 
 # Ensure it's UTF-8 and store the size. E.g. åäö are longer than oao
+# TODO: make this a function too
 givenssidlen = len(givenssid.encode("utf8"))
 
-# 32 bytes should be the maximum SSID length
-if givenssidlen <= 32:
-    print(givenssid, "is", givenssidlen, "long and thus valid length")
-else:
-    print("Too long, try again")
+# Checking the SSID length is done twice, so thus a function
+def checkssidlen(givenssidlen):
+    # 32 bytes should be the maximum SSID length
+    if givenssidlen <= 32:
+        print(givenssid, "is", givenssidlen, "long and thus valid length")
+    else:
+        print(givenssid, "is", givenssidlen, "to long, please select a different SSID")
+
+# Checking the SSID without _nomap
+checkssidlen(givenssidlen)
+# TODO: make this a function too
+givenssidlen = len(givenssid.encode("utf8"))
+
+# Continuing with _nomap to ensure it also fits
+givenssid = givenssid + "_nomap"
+checkssidlen(givenssidlen)
