182d1dcae89ed77391daf9807ff04a70c141d5fb
Aminda Suomalainen <suomalainen+git@mikaela.info>
2022-03-18 13:41:42 +0200

    merge flatpak-override-themes.bash into flatpak-workarounds.bash
    I cannot find an issue for those, I think it's by design and the
    internet is full of that solution, except commonly rw instead of ro

--- bash/flatpak-override-themes.bash
+++ bash/flatpak-override-themes.bash
@@ -1,13 +1,1 @@
-#!/usr/bin/env bash
-
-# Show commands being executed
-set -x
-
-# Results in /var/lib/flatpak/overrides/global
-flatpak override --filesystem=/usr/share/themes:ro --filesystem=~/.themes:ro
-flatpak override --filesystem=/usr/share/icons:ro --filesystem=~/.icons:ro
-flatpak override --filesystem=/usr/share/cursors:ro --filesystem=~/.cursors:ro
-flatpak override --show
-
-# Hide commands being executed again
-set +x
+flatpak-workarounds.bash
--- bash/flatpak-workarounds.bash
+++ bash/flatpak-workarounds.bash
@@ -4,10 +4,15 @@
 set -x
 
 # Workarounds for common flatpak app issues
+
+# Allowing system themes in flatpaks
+flatpak override --filesystem=/usr/share/themes:ro --filesystem=~/.themes:ro
+flatpak override --filesystem=/usr/share/icons:ro --filesystem=~/.icons:ro
+flatpak override --filesystem=/usr/share/cursors:ro --filesystem=~/.cursors:ro
+
 # Backticks and a lot of common characters in all Flatpaks
 # https://github.com/flatpak/flatpak/issues/2031
 flatpak override --talk-name=org.fcitx.Fcitx --talk-name=org.freedesktop.portal.Fcitx
-
 
 # https://github.com/flathub/org.briarproject.Briar/issues/5
 flatpak override org.briarproject.Briar --filesystem=~/.briar:create
