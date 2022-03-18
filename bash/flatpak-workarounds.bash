7799b8dd5053de215804f8392f538690cd227b7e
Aminda Suomalainen <suomalainen+git@mikaela.info>
2022-03-18 12:58:13 +0200

    flatpak-workarounds.bash: global workaround for backticks/fcitx
    mentioned in c8bdace60d37b05307c43704785cb080a0fc6aad

--- bash/flatpak-workarounds.bash
+++ bash/flatpak-workarounds.bash
@@ -4,6 +4,11 @@
 set -x
 
 # Workarounds for common flatpak app issues
+# Backticks and a lot of common characters in all Flatpaks
+# https://github.com/flatpak/flatpak/issues/2031
+flatpak override --talk-name=org.fcitx.Fcitx --talk-name=org.freedesktop.portal.Fcitx
+
+
 # https://github.com/flathub/org.briarproject.Briar/issues/5
 flatpak override org.briarproject.Briar --filesystem=~/.briar:create
 flatpak override org.briarproject.Briar --filesystem=~/.java/.userPrefs/org/briarproject/briar:create
