bebbce9d654d437bc06d5a8fca50576fe194c5b1
Aminda Suomalainen <suomalainen+git@mikaela.info>
2022-03-18 12:40:33 +0200

    flatpak-override-themes.bash: fix typo in comment

--- bash/flatpak-override-themes.bash
+++ bash/flatpak-override-themes.bash
@@ -3,7 +3,7 @@
 # Show commands being executed
 set -x
 
-# resutls in /var/lib/flatpak/overrides/global
+# Results in /var/lib/flatpak/overrides/global
 flatpak override --filesystem=/usr/share/themes:ro --filesystem=~/.themes:ro
 flatpak override --filesystem=/usr/share/icons:ro --filesystem=~/.icons:ro
 flatpak override --filesystem=/usr/share/cursors:ro --filesystem=~/.cursors:ro
