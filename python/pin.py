cbdb39e07d948f55d7e111aa96f88abed23d891a
Aminda Suomalainen <suomalainen+git@mikaela.info>
2022-03-18 17:59:07 +0200

    python/pin.py: initial commit

---
+++ python/pin.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+import secrets
+
+try:
+ wantedCount = int(sys.argv[1])
+except IndexError as noarg:
+  print("Enter a digit as an argument!")
+
+try:
+  for i in range(int(wantedCount)):
+    print(secrets.randbelow(10),end="")
+except NameError as noWantedCount:
+    print()
+print()
