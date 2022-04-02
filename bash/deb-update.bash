281cf1b07b86ae777c39127a753c36e2ffdc8691
Aminda Suomalainen <suomalainen+git@mikaela.info>
2022-04-03 01:39:17 +0300

    deb-update.bash: switch to en_US.utf8 and comment-lecture for three lines

--- bash/deb-update.bash
+++ bash/deb-update.bash
@@ -5,8 +5,10 @@
 # Show commands being executed
 set -x
 
-# Ensure standard/English
-export LANG=C
+# English (US) is generally installed everywhere, flatpak and packagekit use
+# weird glyphs unavailable in C and C.utf8 is a RedHattism, which Debian
+# side of the family doesn't recognise.
+export LANG=en_US.utf8
 
 # if hash checks that the command exists
 if hash apt-get 2>/dev/null; then
