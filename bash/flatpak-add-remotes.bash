0a2297537a23a09feaedc4ff2852866d90ee60a2
Aminda Suomalainen <suomalainen+git@mikaela.info>
2022-04-02 19:39:03 +0300

    flatpak-add-remotes.bash: add nheko-nightly

--- bash/flatpak-add-remotes.bash
+++ bash/flatpak-add-remotes.bash
@@ -8,6 +8,7 @@ set -x
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 flatpak remote-add --if-not-exists kdeapps https://distribute.kde.org/kdeapps.flatpakrepo
+flatpak remote-add --if-not-exists nheko-nightly https://nheko.im/nheko-reborn/nheko/-/raw/master/nheko-nightly.flatpakrepo
 
 # Hide commands being executed again
 set +x
