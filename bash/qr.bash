8162696f55998f751067791c8c4004cc3fb1979a
Aminda Suomalainen <suomalainen+git@mikaela.info>
2023-01-31 10:58:29 +0200

    qr.bash: double the pixel size

--- bash/qr.bash
+++ bash/qr.bash
@@ -3,13 +3,14 @@
 # Arguments: Name & content
 
 # -l H uses the highest error correction in case 30 % gets damaged (or artistried upon)
+# -s 6 doubles the dot/pixel size
 
 # Outputs QR code to terminal
 
 # ${@:2} takes all arguments except not the first one
 qrencode -l H -t utf8 "${@:2}"
 # Stores QR code as $1.png (first argument)
-qrencode -l H -o "$1".png "${@:2}"
+qrencode -s 6 -l H -o "$1".png "${@:2}"
 # My stepfather wants jpg
 convert "$1".png "$1".jpg
 # Prints details
