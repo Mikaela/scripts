4ea1bcdb4057e4dc0e4a9c44b07a8d873aa016f6
Aminda Suomalainen <suomalainen+git@mikaela.info>
2023-01-31 10:49:28 +0200

    qr.bash: add conversion to jpg

--- bash/qr.bash
+++ bash/qr.bash
@@ -10,7 +10,8 @@
 qrencode -l H -t utf8 "${@:2}"
 # Stores QR code as $1.png (first argument)
 qrencode -l H -o "$1".png "${@:2}"
-
+# My stepfather wants jpg
+convert "$1".png "$1".jpg
 # Prints details
 echo "Name: $1.png"
 echo "Content: ${@:2}"
