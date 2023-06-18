43ba89861ed17852c14ed76925f225c5495da552
Aminda Suomalainen <suomalainen+git@mikaela.info>
2023-06-18 09:43:07 +0300

    begin writing python/keyoxide_gen_hashed_proof.py

---
+++ python/keyoxide_gen_hashed_proof.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This script generated Keyoxide hashed proofs in argon2id and bcrypt
+"""
+
+PROOF = str(input("Enter the proof: "))
+
+print("$argon2id$v=19$m=64,t=512,p=2$mDejbh8SmFFJPjbjf+u9kQ$yQpomARjYKfgEDAZ1jdgnA")
+
+
+print("$2a$11$kBchZf6QkDDHNhZhd8s7iOcXCuqAz3Qawd5eG5A6RygTtDfeZ9a/y")
