662503876b0ef50b7007f31f9928de447d3b5d01
Aminda Suomalainen <suomalainen+git@mikaela.info>
2023-10-08 19:49:21 +0300

    add experimental/untested wifiqr-open.bash

---
+++ bash/wifiqr-open.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Argument: SSID
+
+# -l H uses the highest error correction in case 30 % gets damaged (or artistried upon)
+
+PIXELSIZE=6
+
+# Outputs QR code to terminal
+qrencode -l H -t utf8 "WIFI:T:nopass;S:$1;;"
+# Stores QR code as SSID.png, -s doubles pixel size from default 3
+qrencode -l H -s $PIXELSIZE -o $1.png "WIFI:T:nopass;S:$1;;"
+# Might as well jpg this too
+convert "$1".png "$1".jpg
+
+# Prints details
+echo "SSID: $1"
+echo "Stored as $1.png"
