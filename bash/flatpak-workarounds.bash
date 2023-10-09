4d9b3ff880ae1639d2a702f4860087bb07f0513c
Aminda Suomalainen <suomalainen+git@mikaela.info>
2023-10-09 16:16:52 +0300

    flatpak-workarounds.bash: merge global dotfiles overrides

--- bash/flatpak-workarounds.bash
+++ bash/flatpak-workarounds.bash
@@ -8,20 +8,12 @@ set -x
 # Reserved directories that cannot be shared
 # to flatpaks include at least: /usr
 
-# In case debugging flatpaks is subject to the other restrictions and running
-# gdb there is made easier by access to the file. By same logic, they need
-# to be able to write to the log.
-flatpak override --filesystem=~/.gdbinit:ro --filesystem=~/gdb.txt $@
-
-# Allow read-only access to ~/.curlrc in case a flatpak is utilising curl
-# or libcurl starts reading it
-flatpak override --filesystem=~/.curlrc:ro $@
-
-# Allow ro-access to user-specific fonts
-flatpak override --filesystem=~/.local/share/fonts:ro $@
-
-# Text-editors appreciate this
-flatpak override --filesystem=~/.editorconfig:ro $@
+# Overrides for all apps improving Quality of Life
+# - debugging with gdb
+# - curl configuration following
+# - text editor configuration
+# - user-specific fonts
+flatpak override --filesystem=~/.gdbinit:ro --filesystem=~/gdb.txt --filesystem=~/.curlrc:ro --filesystem=~/.editorconfig:ro --filesystem=~/.local/share/fonts:ro $@
 
 # Public git repositories I access or symlink much.
 # TODO: scripts & text-editors (otherwise nvim and code and codium cannot edit them!)
@@ -33,9 +25,6 @@ flatpak override --talk-name=org.fcitx.Fcitx --talk-name=org.freedesktop.portal.
 
 # https://github.com/flathub/org.briarproject.Briar/issues/5
 flatpak override org.briarproject.Briar --filesystem=~/.briar:create --filesystem=~/.java/.userPrefs/org/briarproject/briar:create $@
-
-# email signature
-#flatpak override org.claws_mail.Claws-Mail --filesystem=~/.signature:create $@
 
 # Directory I have normal Nextcloud in
 flatpak override com.nextcloud.desktopclient.nextcloud --filesystem=~/Nextcloud:create $@
