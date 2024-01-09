0e7aa2b051d2314e1b7abf162d849b32fb79f304
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-01-09 08:03:44 +0200

    flatpakifier.bash: use variables

--- bash/flatpakifier.bash
+++ bash/flatpakifier.bash
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 # This script partially reverts https://gitea.blesmrt.net/mikaela/scripts/commit/e76a4a6a0d0472428d519205543171b1b1cdc258
+
+export FLATPAKEXPORTS=/var/lib/flatpak/exports/bin
+export TARGETDIR=~/.local/bin
+
 set -x
-ln -sf /var/lib/flatpak/exports/bin/com.rafaelmardojai.Blanket ~/.local/bin/blanket
+ln -sf $FLATPAKEXPORTS/com.rafaelmardojai.Blanket $TARGETDIR/blanket
 # `flatpak make-current org.briarproject.Briar stable`?
-ln -sf /var/lib/flatpak/exports/bin/org.briarproject.Briar ~/.local/bin/briar
-ln -sf /var/lib/flatpak/exports/bin/org.claws_mail.Claws-Mail ~/.local/bin/claws-mail
-ln -sf /var/lib/flatpak/exports/bin/com.visualstudio.code ~/.local/bin/code
-ln -sf /var/lib/flatpak/exports/bin/com.vscodium.codium ~/.local/bin/codium
-ln -sf /var/lib/flatpak/exports/bin/chat.delta.desktop ~/.local/bin/deltachat
-ln -sf /var/lib/flatpak/exports/bin/im.riot.Riot ~/.local/bin/element
-ln -sf /var/lib/flatpak/exports/bin/com.github.tchx84.Flatseal ~/.local/bin/flatseal
-ln -sf /var/lib/flatpak/exports/bin/chat.schildi.desktop ~/.local/bin/schildichat
-ln -sf /var/lib/flatpak/exports/bin/org.signal.Signal ~/.local/bin/signal-desktop
-ln -sf /var/lib/flatpak/exports/bin/org.telegram.desktop ~/.local/bin/telegram-desktop
-ln -sf /var/lib/flatpak/exports/bin/com.github.micahflee.torbrowser-launcher ~/.local/bin/torbrowser-launcher-flatpak
-ln -sf /var/lib/flatpak/exports/bin/org.videolan.VLC ~/.local/bin/vlc-flatpak
-ln -sf /var/lib/flatpak/exports/bin/net.davidotek.pupgui2 ~/.local/bin/protonup
+ln -sf $FLATPAKEXPORTS/org.briarproject.Briar $TARGETDIR/briar
+ln -sf $FLATPAKEXPORTS/org.claws_mail.Claws-Mail $TARGETDIR/claws-mail
+ln -sf $FLATPAKEXPORTS/com.visualstudio.code $TARGETDIR/code
+ln -sf $FLATPAKEXPORTS/com.vscodium.codium $TARGETDIR/codium
+ln -sf $FLATPAKEXPORTS/chat.delta.desktop $TARGETDIR/deltachat
+ln -sf $FLATPAKEXPORTS/im.riot.Riot $TARGETDIR/element
+ln -sf $FLATPAKEXPORTS/com.github.tchx84.Flatseal $TARGETDIR/flatseal
+ln -sf $FLATPAKEXPORTS/chat.schildi.desktop $TARGETDIR/schildichat
+ln -sf $FLATPAKEXPORTS/org.signal.Signal $TARGETDIR/signal-desktop
+ln -sf $FLATPAKEXPORTS/org.telegram.desktop $TARGETDIR/telegram-desktop
+ln -sf $FLATPAKEXPORTS/com.github.micahflee.torbrowser-launcher $TARGETDIR/torbrowser-launcher-flatpak
+ln -sf $FLATPAKEXPORTS/org.videolan.VLC $TARGETDIR/vlc-flatpak
+ln -sf $FLATPAKEXPORTS/net.davidotek.pupgui2 $TARGETDIR/protonup
 set +x
