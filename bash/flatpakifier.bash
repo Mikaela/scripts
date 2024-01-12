49a971b5b46bdf6a43789d27ec4fd0077725ced2
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-01-12 10:26:39 +0200

    flatpakifier.bash: comment those that got wrappers again
    Ref: 84015e1dbbead240489efcbc3a7aff4e2affb246

--- bash/flatpakifier.bash
+++ bash/flatpakifier.bash
@@ -9,15 +9,19 @@ ln -sf $FLATPAKEXPORTS/com.rafaelmardojai.Blanket $TARGETDIR/blanket
 # `flatpak make-current org.briarproject.Briar stable`?
 ln -sf $FLATPAKEXPORTS/org.briarproject.Briar $TARGETDIR/briar
 ln -sf $FLATPAKEXPORTS/org.claws_mail.Claws-Mail $TARGETDIR/claws-mail
-ln -sf $FLATPAKEXPORTS/com.visualstudio.code $TARGETDIR/code
-ln -sf $FLATPAKEXPORTS/com.vscodium.codium $TARGETDIR/codium
-ln -sf $FLATPAKEXPORTS/chat.delta.desktop $TARGETDIR/deltachat
-ln -sf $FLATPAKEXPORTS/im.riot.Riot $TARGETDIR/element
 ln -sf $FLATPAKEXPORTS/com.github.tchx84.Flatseal $TARGETDIR/flatseal
-ln -sf $FLATPAKEXPORTS/chat.schildi.desktop $TARGETDIR/schildichat
-ln -sf $FLATPAKEXPORTS/org.signal.Signal $TARGETDIR/signal-desktop
+
 ln -sf $FLATPAKEXPORTS/org.telegram.desktop $TARGETDIR/telegram-desktop
 ln -sf $FLATPAKEXPORTS/com.github.micahflee.torbrowser-launcher $TARGETDIR/torbrowser-launcher-flatpak
 ln -sf $FLATPAKEXPORTS/org.videolan.VLC $TARGETDIR/vlc-flatpak
 ln -sf $FLATPAKEXPORTS/net.davidotek.pupgui2 $TARGETDIR/protonup
+
+# Electron apps that actually need wrappers and flags
+#ln -sf $FLATPAKEXPORTS/com.visualstudio.code $TARGETDIR/code
+#ln -sf $FLATPAKEXPORTS/com.vscodium.codium $TARGETDIR/codium
+#ln -sf $FLATPAKEXPORTS/chat.delta.desktop $TARGETDIR/deltachat
+#ln -sf $FLATPAKEXPORTS/im.riot.Riot $TARGETDIR/element
+#ln -sf $FLATPAKEXPORTS/chat.schildi.desktop $TARGETDIR/schildichat
+#ln -sf $FLATPAKEXPORTS/org.signal.Signal $TARGETDIR/signal-desktop
+
 set +x
