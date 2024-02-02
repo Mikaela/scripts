2e42459e1ad12f8a88e2086b3049610443292030
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-02-02 13:45:05 +0200

    adb-permissions.bash: comment others than logfox under suspicion of contributing to reboot issue

--- bash/adb-permissions.bash
+++ bash/adb-permissions.bash
@@ -14,22 +14,22 @@ set -x
 echo "In case of trouble check Android System Settings, Connected devices, USB mode. It should be data transfer for adb USB debugging to take effect."
 
 # DetoxDroid, https://github.com/flxapps/DetoxDroid/wiki/Manual-Installation
-adb "$@" shell pm grant com.flx_apps.digitaldetox android.permission.WRITE_SECURE_SETTINGS
-adb "$@" shell dpm set-device-owner com.flx_apps.digitaldetox/.DetoxDroidDeviceAdminReceiver
+#adb "$@" shell pm grant com.flx_apps.digitaldetox android.permission.WRITE_SECURE_SETTINGS
+#adb "$@" shell dpm set-device-owner com.flx_apps.digitaldetox/.DetoxDroidDeviceAdminReceiver
 #echo "Remember to force-stop & restart DetoxDroid!"
 
 # Greenify, https://web.archive.org/web/20190209023358/https://greenify.uservoice.com/knowledgebase/articles/749142-how-to-grant-permissions-required-by-some-features
 # TODO: Alive source
-adb "$@" shell pm grant com.oasisfeng.greenify android.permission.WRITE_SECURE_SETTINGS
-adb "$@" shell pm grant com.oasisfeng.greenify android.permission.DUMP
-adb "$@" shell pm grant com.oasisfeng.greenify android.permission.READ_LOGS
-adb "$@" shell pm grant com.oasisfeng.greenify android.permission.GET_APP_OPS_STATS
+#adb "$@" shell pm grant com.oasisfeng.greenify android.permission.WRITE_SECURE_SETTINGS
+#adb "$@" shell pm grant com.oasisfeng.greenify android.permission.DUMP
+#adb "$@" shell pm grant com.oasisfeng.greenify android.permission.READ_LOGS
+#adb "$@" shell pm grant com.oasisfeng.greenify android.permission.GET_APP_OPS_STATS
 #echo "Remember to force-stop & restart Greenify!"
 
 # Nevolution, https://nevo.app/setup.html
-adb "$@" shell setprop persist.log.tag.NotificationService DEBUG
-adb "$@" shell pm grant com.oasisfeng.nevo android.permission.READ_LOGS
-adb "$@" shell pm grant com.oasisfeng.nevo android.permission.INTERACT_ACROSS_USERS
+#adb "$@" shell setprop persist.log.tag.NotificationService DEBUG
+#adb "$@" shell pm grant com.oasisfeng.nevo android.permission.READ_LOGS
+#adb "$@" shell pm grant com.oasisfeng.nevo android.permission.INTERACT_ACROSS_USERS
 #echo "Remember to force-stop & restart Nevolution!"
 
 # Logfox adb reader from F-Droid
@@ -37,6 +37,6 @@ adb "$@" shell pm grant com.f0x1d.logfox android.permission.READ_LOGS
 
 # Wait a bit and then reboot the device
 sleep 5
-adb "$@" reboot
+#adb "$@" reboot
 
 set +x
