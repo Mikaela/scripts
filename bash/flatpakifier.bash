4ff958c6c8d691085029c8018243647fb40f2950
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-02-24 09:32:17 +0200

    flatpakifier.bash: fix torbrowser-launcher id

--- bash/flatpakifier.bash
+++ bash/flatpakifier.bash
@@ -12,7 +12,7 @@ ln -sf $FLATPAKEXPORTS/org.claws_mail.Claws-Mail $TARGETDIR/claws-mail
 ln -sf $FLATPAKEXPORTS/com.github.tchx84.Flatseal $TARGETDIR/flatseal
 
 ln -sf $FLATPAKEXPORTS/org.telegram.desktop $TARGETDIR/telegram-desktop
-ln -sf $FLATPAKEXPORTS/com.github.micahflee.torbrowser-launcher $TARGETDIR/torbrowser-launcher-flatpak
+ln -sf $FLATPAKEXPORTS/org.torproject.torbrowser-launcher $TARGETDIR/torbrowser-launcher-flatpak
 ln -sf $FLATPAKEXPORTS/org.videolan.VLC $TARGETDIR/vlc-flatpak
 ln -sf $FLATPAKEXPORTS/net.davidotek.pupgui2 $TARGETDIR/protonup
 
