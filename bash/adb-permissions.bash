ed43e5ce06306739cf12d20ca33bdff672f30935
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-04-18 15:48:06 +0300

    adb-permissions.bash: remove Greenify & Nevo
    Propietary, dead? At least Greenify is known to cause reboots on recent Androids

--- bash/adb-permissions.bash
+++ bash/adb-permissions.bash
@@ -18,20 +18,6 @@ echo "In case of trouble check Android System Settings, Connected devices, USB m
 #adb "$@" shell dpm set-device-owner com.flx_apps.digitaldetox/.DetoxDroidDeviceAdminReceiver
 #echo "Remember to force-stop & restart DetoxDroid!"
 
-# Greenify, https://web.archive.org/web/20190209023358/https://greenify.uservoice.com/knowledgebase/articles/749142-how-to-grant-permissions-required-by-some-features
-# TODO: Alive source
-#adb "$@" shell pm grant com.oasisfeng.greenify android.permission.WRITE_SECURE_SETTINGS
-#adb "$@" shell pm grant com.oasisfeng.greenify android.permission.DUMP
-#adb "$@" shell pm grant com.oasisfeng.greenify android.permission.READ_LOGS
-#adb "$@" shell pm grant com.oasisfeng.greenify android.permission.GET_APP_OPS_STATS
-#echo "Remember to force-stop & restart Greenify!"
-
-# Nevolution, https://nevo.app/setup.html
-#adb "$@" shell setprop persist.log.tag.NotificationService DEBUG
-#adb "$@" shell pm grant com.oasisfeng.nevo android.permission.READ_LOGS
-#adb "$@" shell pm grant com.oasisfeng.nevo android.permission.INTERACT_ACROSS_USERS
-#echo "Remember to force-stop & restart Nevolution!"
-
 # Logfox adb reader from F-Droid
 adb "$@" shell pm grant com.f0x1d.logfox android.permission.READ_LOGS
 
