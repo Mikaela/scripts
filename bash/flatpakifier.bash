6d3e3dec230c40eafc273d9b42ae8561503e0b5e
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-06-05 09:34:43 +0300

    telegram-desktop wrapper for 64Gram

--- bash/flatpakifier.bash
+++ bash/flatpakifier.bash
@@ -13,7 +13,7 @@ ln -sfv $FLATPAKEXPORTS/com.rafaelmardojai.Blanket $TARGETDIR/blanket
 ln -sfv $FLATPAKEXPORTS/org.claws_mail.Claws-Mail $TARGETDIR/claws-mail
 ln -sfv $FLATPAKEXPORTS/com.github.tchx84.Flatseal $TARGETDIR/flatseal
 
-ln -sfv $FLATPAKEXPORTS/org.telegram.desktop $TARGETDIR/telegram-desktop
+#ln -sfv $FLATPAKEXPORTS/org.telegram.desktop $TARGETDIR/telegram-desktop
 ln -sfv $FLATPAKEXPORTS/org.torproject.torbrowser-launcher $TARGETDIR/torbrowser-launcher-flatpak
 ln -sfv $FLATPAKEXPORTS/org.videolan.VLC $TARGETDIR/vlc-flatpak
 ln -sfv $FLATPAKEXPORTS/net.davidotek.pupgui2 $TARGETDIR/protonup
