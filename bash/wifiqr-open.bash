5d871bf3a4c825b1fcb026f8a396227589ee0849
Aminda Suomalainen <suomalainen+git@mikaela.info>
2024-07-15 11:08:22 +0300

    wifiqr*.bash: specify H:false
    As a very basic form of hidden network awareness

--- bash/wifiqr-open.bash
+++ bash/wifiqr-open.bash
@@ -2,14 +2,15 @@
 
 # Argument: SSID
 
-# -l H uses the highest error correction in case 30 % gets damaged (or artistried upon)
+# -l H uses the highest error correction in case 30 % gets damaged
+# (or gets art upon it)
 
 PIXELSIZE=6
 
 # Outputs QR code to terminal
 qrencode -l H -t utf8 "WIFI:T:nopass;S:$1;;"
 # Stores QR code as SSID.png, -s doubles pixel size from default 3
-qrencode -l H -s $PIXELSIZE -o $1.png "WIFI:T:nopass;S:$1;;"
+qrencode -l H -s $PIXELSIZE -o $1.png "WIFI:T:nopass;S:$1;;H:false;"
 # Might as well jpg this too
 magick "$1".png "$1".jpg
 
--- bash/wifiqr.bash
+++ bash/wifiqr.bash
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-# Arguments: SSID and WPA2 (3 untested) passphrase
+# Arguments: SSID and WPA2 passphrase
 
-# -l H uses the highest error correction in case 30 % gets damaged (or artistried upon)
+# -l H uses the highest error correction in case 30 % gets damaged
+# (or gets art upon it)
 
 PIXELSIZE=6
 
 # Outputs QR code to terminal
-qrencode -l H -t utf8 "WIFI:T:WPA;S:$1;P:$2;;"
+qrencode -l H -t utf8 "WIFI:T:WPA;S:$1;P:$2;H:false;"
 # Stores QR code as SSID.png, -s doubles pixel size from default 3
-qrencode -l H -s $PIXELSIZE -o $1.png "WIFI:T:WPA;S:$1;P:$2;;"
+qrencode -l H -s $PIXELSIZE -o $1.png "WIFI:T:WPA;S:$1;P:$2;H:false;"
 # Might as well jpg this too
 magick "$1".png "$1".jpg
 
