c344db12a27b58c20e659fb805f5765b5c7e9f3b
Aminda Suomalainen <suomalainen@aminda.eu>
2025-05-21 11:52:59 +0300

    Revert "flatpak-workarounds.bash: let Steam manage xdg-config/autostart"
    This reverts commit bccd86cf5778cdecb96926a91b94dc3997572b60.

--- bash/flatpak-workarounds.bash
+++ bash/flatpak-workarounds.bash
@@ -126,8 +126,6 @@ flatpak override com.valvesoftware.Steam --system-talk-name=org.freedesktop.Cons
 flatpak override com.valvesoftware.Steam --filesystem=xdg-desktop:rw $@
 flatpak override com.valvesoftware.Steam --filesystem=xdg-data/applications:rw $@
 flatpak override com.valvesoftware.Steam --filesystem=xdg-data/icons:rw $@
-# what if I just let Steam manage autostart?
-flatpak override com.valvesoftware.Steam --filesystem=xdg-config/autostart:rw $@
 
 # Place the wrapper script the desktop entries above expect
 # TODO: Be less lazy and don't nest if
