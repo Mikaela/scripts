dca4e698e5dac6a9a8aa610146fed02b6cbe1c41
Aminda Suomalainen <suomalainen@aminda.eu>
2025-06-19 08:10:02 +0300

    flatpak-install-nightly-kdeapps.bash: add neochat, plasmatube, kteatime & konversation

--- bash/flatpak-install-nightly-kdeapps.bash
+++ bash/flatpak-install-nightly-kdeapps.bash
@@ -6,4 +6,9 @@ flatpak install --or-update https://cdn.kde.org/flatpak/kde-runtime-nightly/org.
 #flatpak install --or-update https://cdn.kde.org/flatpak/kate-nightly/org.kde.kate.flatpakref $@
 flatpak install --or-update https://cdn.kde.org/flatpak/haruna-nightly/org.kde.haruna.flatpakref $@
 flatpak install --or-update https://cdn.kde.org/flatpak/audiotube-nightly/org.kde.audiotube.flatpakref
+flatpak install --or-update https://cdn.kde.org/flatpak/neochat-nightly/org.kde.neochat.flatpakref $@
+flatpak install --or-update https://cdn.kde.org/flatpak/plasmatube-nightly/org.kde.plasmatube.flatpakref $@
+flatpak install --or-update https://cdn.kde.org/flatpak/kteatime-nightly/org.kde.kteatime.flatpakref $@
+flatpak install --or-update https://cdn.kde.org/flatpak/konversation-nightly/org.kde.konversation.flatpakref $@
+
 set +x
