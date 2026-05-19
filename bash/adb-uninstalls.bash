cc44bf86d75abce213ac106e1af5aa45fb5ad67b
Aminda Suomalainen <suomalainen@aminda.eu>
2026-05-19 14:00:44 +0300

    adb-uninstalls.bash: comment out the weird Android Auto thingy

--- bash/adb-uninstalls.bash
+++ bash/adb-uninstalls.bash
@@ -42,17 +42,17 @@ echo "In case of trouble check Android System Settings, Connected devices, USB m
 (adb "$@" shell pm disable-user com.evenwell.powersaving.g3 &)
 (adb "$@" shell pm disable-user com.evenwell.emm &)
 
-# Keep last! Weird workaround for Finnish language in Android Auto audio input https://www.youtube.com/watch?v=jXEqNDaJJ00
-(adb "$@" shell pm uninstall com.google.android.googlequicksearchbox &)
-#(adb "$@" shell pm disable com.google.android.googlequicksearchbox &)
-(adb "$@" shell pm uninstall com.google.android.projection.gearhead &)
-#(adb "$@" shell pm disable com.google.android.projection.gearhead &)
-
+## Keep last! Weird workaround for Finnish language in Android Auto audio input https://www.youtube.com/watch?v=jXEqNDaJJ00
+#(adb "$@" shell pm uninstall com.google.android.googlequicksearchbox &)
+##(adb "$@" shell pm disable com.google.android.googlequicksearchbox &)
+#(adb "$@" shell pm uninstall com.google.android.projection.gearhead &)
+##(adb "$@" shell pm disable com.google.android.projection.gearhead &)
+#
 # Wait a bit and then reboot the device
-echo "You may want to reinstall Google first and Android Auto after it."
-echo "https://play.google.com/store/apps/details?id=com.google.android.googlequicksearchbox"
-echo "https://play.google.com/store/apps/details?id=com.google.android.projection.gearhead"
-echo "Remember to not allow Android Auto to adjust system settings!"
+#echo "You may want to reinstall Google first and Android Auto after it."
+#echo "https://play.google.com/store/apps/details?id=com.google.android.googlequicksearchbox"
+#echo "https://play.google.com/store/apps/details?id=com.google.android.projection.gearhead"
+#echo "Remember to not allow Android Auto to adjust system settings!"
 
 sleep 15
 echo "Please execute \"adb $@ reboot\" once finished."
