0d7f7170100cff123be20177bac6848322dd3e17
Aminda Suomalainen <suomalainen@aminda.eu>
2026-07-13 14:41:01 +0300

    flatpak-install-nightly-kdeapps.bash: add a missing

--- bash/flatpak-install-nightly-kdeapps.bash
+++ bash/flatpak-install-nightly-kdeapps.bash
@@ -6,7 +6,7 @@ set -x
 flatpak install --or-update https://cdn.kde.org/flatpak/kde-runtime-nightly/org.kde.Platform.flatpakref $@
 #flatpak install --or-update https://cdn.kde.org/flatpak/kate-nightly/org.kde.kate.flatpakref $@
 flatpak install --or-update https://cdn.kde.org/flatpak/haruna-nightly/org.kde.haruna.flatpakref $@
-flatpak install --or-update https://cdn.kde.org/flatpak/audiotube-nightly/org.kde.audiotube.flatpakref
+flatpak install --or-update https://cdn.kde.org/flatpak/audiotube-nightly/org.kde.audiotube.flatpakref $@
 flatpak install --or-update https://cdn.kde.org/flatpak/neochat-nightly/org.kde.neochat.flatpakref $@
 flatpak install --or-update https://cdn.kde.org/flatpak/plasmatube-nightly/org.kde.plasmatube.flatpakref $@
 flatpak install --or-update https://cdn.kde.org/flatpak/kteatime-nightly/org.kde.kteatime.flatpakref $@
